9fa44d20aafb9566ef4a4b83c4b498e3993bd31d
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-07-29 15:11:18 +0100

    fix: deploy error

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,7 +5,11 @@ import useAuth from "../../hooks/useAuth";
 import Navbar from "../Navbar";
 import AuthModal from "../Modal/Auth";
 
-const Layout: React.FC = ({ children }: any) => {
+type Props = {
+  children?: React.ReactNode;
+};
+
+const Layout: React.FC<Props> = ({ children }) => {
   // useAuth(); // will implement later at end of tutorial
 
   return (
