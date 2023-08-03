9eb403b4bc9f24d94ffba6b53e7e4f2e9e7982db
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-08-03 11:06:25 +0100

    update doc metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400&display=swap"
-        rel="stylesheet"
-      />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
+      <link rel="icon" type="image/svg+xml" href="/images/Logo.svg" />
       <Head />
       <body>
         <Main />
