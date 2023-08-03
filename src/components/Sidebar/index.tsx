6adc0a7fa55698dd280bb8423097b6011e149113
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-08-03 17:29:24 +0100

    width

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -19,7 +19,7 @@ const Sidebar: React.FC<Props> = ({ open, toggleMenu }) => {
       padding={1}
       m={1}
       spacing={5}
-      width={"full"}
+      width={open ? "full" : 0}
       alignItems="center"
       transition={"all 0.2s"}
       p={2}
