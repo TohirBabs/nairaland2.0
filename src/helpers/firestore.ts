48f0b00afb0246f543fbc4233cea41dab16c4006
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-08-03 11:09:28 +0100

    apply short time format

--- src/helpers/firestore.ts
+++ src/helpers/firestore.ts
@@ -1,5 +1,6 @@
 import { user } from "firebase-functions/v1/auth";
 import { query, collection, getDocs } from "firebase/firestore";
+import moment from "moment";
 import { firestore } from "../firebase/clientApp";
 
 export const getMySnippets = async (userId: string) => {
@@ -10,3 +11,22 @@ export const getMySnippets = async (userId: string) => {
   const snippetDocs = await getDocs(snippetQuery);
   return snippetDocs.docs.map((doc) => ({ ...doc.data() }));
 };
+
+moment.locale("en", {
+  relativeTime: {
+    future: "in %s",
+    past: "%s ago",
+    s: "1s",
+    ss: "%ss",
+    m: "1m",
+    mm: "%dm",
+    h: "1h",
+    hh: "%dh",
+    d: "1d",
+    dd: "%dd",
+    M: "1M",
+    MM: "%dM",
+    y: "1Y",
+    yy: "%dY",
+  },
+});
