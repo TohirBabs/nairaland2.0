484ee3ff7cf93dc808d5706d302cddda62b64615
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-08-05 19:56:42 +0100

    button styles

--- src/chakra/button.ts
+++ src/chakra/button.ts
@@ -2,8 +2,8 @@ import type { ComponentStyleConfig } from "@chakra-ui/theme";
 
 export const Button: ComponentStyleConfig = {
   baseStyle: {
-    borderRadius: "2rem",
-    fontSize: "1rem",
+    borderRadius: "1.2rem",
+    fontSize: "0.9rem",
     fontWeight: 700,
     _focus: {
       boxShadow: "none",
@@ -11,33 +11,33 @@ export const Button: ComponentStyleConfig = {
   },
   sizes: {
     sm: {
-      fontSize: "1rem",
+      fontSize: "0.9rem",
     },
     md: {
       fontSize: "1rem",
       // height: "28px",
     },
   },
-  variants: {
-    solid: {
-      color: "white",
-      bg: "green.500",
-      _hover: {
-        bg: "green.400",
-      },
-    },
-    outline: {
-      color: "green.500",
-      border: "1px solid",
-      borderColor: "green.500",
-    },
-    oauth: {
-      height: "34px",
-      border: "1px solid",
-      borderColor: "gray.300",
-      _hover: {
-        bg: "gray.50",
-      },
-    },
-  },
+  // variants: {
+  //   solid: {
+  //     color: "white",
+  //     bg: "green.500",
+  //     _hover: {
+  //       bg: "green.400",
+  //     },
+  //   },
+  //   outline: {
+  //     color: "green.500",
+  //     border: "1px solid",
+  //     borderColor: "green.500",
+  //   },
+  //   oauth: {
+  //     height: "34px",
+  //     border: "1px solid",
+  //     borderColor: "gray.300",
+  //     _hover: {
+  //       bg: "gray.50",
+  //     },
+  //   },
+  // },
 };
