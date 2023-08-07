18db3be45c2d4e428a61f3922f4eec673c665079
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-08-07 13:46:49 +0100

    update

--- src/components/Sidebar/Community/PersonalHome.tsx
+++ src/components/Sidebar/Community/PersonalHome.tsx
@@ -1,44 +1,53 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import { Button, Flex, Icon, Stack, Text } from "@chakra-ui/react";
 import { FaReddit } from "react-icons/fa";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/dist/client/router";
+import { useRecoilValue } from "recoil";
+import useCommunityData from "../../../hooks/useCommunityData";
+import { auth } from "../../../firebase/clientApp";
+import { communityState } from "../../../atoms/communitiesAtom";
+import NewPostForm from "../NewPostForm";
 
 const PersonalHome: React.FC = () => {
+  const [user, loadingUser, error] = useAuthState(auth);
+  const router = useRouter();
+  const { community } = router.query;
+  // const visitedCommunities = useRecoilValue(communityState).visitedCommunities;
+  const communityStateValue = useRecoilValue(communityState);
+  const { loading } = useCommunityData();
+
+  /**
+   * Not sure why not working
+   * Attempting to redirect user if not authenticated
+   */
+  useEffect(() => {
+    if (!user && !loadingUser && communityStateValue.currentCommunity.id) {
+      router.push(`/r/${communityStateValue.currentCommunity.id}`);
+    }
+  }, [user, loadingUser, communityStateValue.currentCommunity]);
+
   return (
     <Flex
       direction="column"
-      bg="white"
-      borderRadius={10}
+      bg="gray.800"
+      color={"white"}
+      borderRadius={20}
       overflow="hidden"
       cursor="pointer"
       border="1px solid"
-      borderColor="gray.300"
-      position="sticky"
+      borderColor="gray.500"
+      width={"full"}
     >
-      <Flex
-        align="flex-end"
-        color="white"
-        p="6px 10px"
-        bg="blue.500"
-        height="34px"
-        borderRadius="4px 4px 0px 0px"
-        fontWeight={600}
-        bgImage="url(/images/redditPersonalHome.png)"
-        backgroundSize="cover"
-      ></Flex>
-      <Flex direction="column" p="12px">
-        <Flex align="center" mb={2}>
-          <Icon as={FaReddit} fontSize={50} color="brand.100" mr={2} />
-          <Text fontWeight={600}>Home</Text>
-        </Flex>
-        <Stack spacing={3}>
-          <Text fontSize="9pt">
-            Your personal Reddit frontpage, built for you.
-          </Text>
-          <Button height="30px">Create Post</Button>
-          <Button variant="outline" height="30px">
-            Create Community
-          </Button>
-        </Stack>
+      <Flex direction="column" p={1}>
+        {user && (
+          <NewPostForm
+            communityId={communityStateValue.currentCommunity.id}
+            communityImageURL={communityStateValue.currentCommunity.imageURL}
+            user={user}
+          />
+        )}
       </Flex>
     </Flex>
   );
